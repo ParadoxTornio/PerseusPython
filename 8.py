e80adbe6d83b2f39da8946008e13401e689e3775
Paradox <paradox.tornio@gmail.com>
2023-07-19 16:43:33 +0300

    0.9

--- 8.py
+++ 8.py
@@ -1,27 +1,19 @@
-plants = [2, 4]
-zombies = [1, 3, 5, 7]
-
-plants_attack = sum(plants)
-zombies_attack = sum(zombies)
-
-plants_counter = 0
-zombies_counter = 0
-
-
 def attack(def_, att_):
-    global plants_counter
-    global zombies_counter
-    for index, value in enumerate(plants):
+    plants_counter = len(def_)
+    zombies_counter = len(att_)
+    plants_attack = sum(def_)
+    zombies_attack = sum(att_)
+    for index, value in enumerate(def_):
         try:
             if def_[index] > att_[index]:
-                plants_counter += 1
+                zombies_counter -= 1
             elif def_[index] == att_[index]:
-                plants_counter += 0
-                zombies_counter += 0
+                plants_counter -= 1
+                zombies_counter -= 1
             else:
-                zombies_counter += 1
+                plants_counter -= 1
         except IndexError:
-            plants_counter += 1
+            zombies_counter -= 1
     if plants_counter == zombies_counter and plants_attack >= zombies_attack:
         return True
     if plants_counter > zombies_counter:
@@ -32,4 +24,7 @@ def attack(def_, att_):
         return False
 
 
-print(attack(plants, zombies))
+print(attack([2, 4, 6, 8], [1, 3, 5, 7]))
+print(attack([2, 4], [1, 3, 5, 7]))
+print(attack([2, 4, 0, 8], [1, 3, 5, 7]))
+print(attack([1, 2, 1, 1], [2, 1, 1, 1]))
