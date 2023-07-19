9e60d8080112a1240d4ced029779d9f1f8e4223d
Paradox <paradox.tornio@gmail.com>
2023-07-19 16:34:05 +0300

    0.9.1

--- 8.py
+++ 8.py
@@ -1,4 +1,4 @@
-plants = [2, 4, 0, 8]
+plants = [2, 4]
 zombies = [1, 3, 5, 7]
 
 plants_attack = sum(plants)
@@ -11,19 +11,25 @@ zombies_counter = 0
 def attack(def_, att_):
     global plants_counter
     global zombies_counter
-    if len(zombies) == len(plants):
-        if def_[0] > att_[0]:
+    for index, value in enumerate(plants):
+        try:
+            if def_[index] > att_[index]:
+                plants_counter += 1
+            elif def_[index] == att_[index]:
+                plants_counter += 0
+                zombies_counter += 0
+            else:
+                zombies_counter += 1
+        except IndexError:
             plants_counter += 1
-        if def_[0] == att_[0]:
-            plants_counter += 0
-            zombies_counter += 0
-        else:
-            zombies_counter += 1
-        if plants_counter == zombies_counter:
-            if plants_attack >= zombies_attack:
-                return True
-        if zombies_attack > plants_attack:
-            return False
+    if plants_counter == zombies_counter and plants_attack >= zombies_attack:
+        return True
+    if plants_counter > zombies_counter:
+        return True
+    if zombies_attack > plants_attack:
+        return False
+    if zombies_counter > plants_counter:
+        return False
 
 
 print(attack(plants, zombies))
